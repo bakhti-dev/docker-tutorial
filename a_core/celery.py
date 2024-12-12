ba4590c203c173d34dfce6dd305ae2fa87f6353b
B <bahtiyorshavkatov@mail.ru>
2024-12-12 11:32:04 +0500

    celery updated

--- a_core/celery.py
+++ a_core/celery.py
@@ -4,5 +4,12 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
 celery_app = Celery('a_core')
+
+# Load settings from Django's settings
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks
 celery_app.autodiscover_tasks()
+
+# Set default concurrency
+celery_app.conf.worker_concurrency = 4
